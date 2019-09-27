0ccbb29b088bc24c412f42680b31983d1a441b83
shoun <shooouuun@gmail.com>
2019-09-28 00:59:18 +0300

    Активация карты и формы. Валидация формы. Подправил функцию подбора нужного существительного

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ var TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var RUSSIAN_WORDS = {flat: 'Квартира', bungalo: 'Бунгало', house: 'Дом', palace: 'Дворец'};
 var PRICES = ['500', '1000', '1500'];
 var ROOMS = ['1', '2', '3', '4', '5', '6', '7'];
+var NOUNS_ROOMS = ['комната', 'комнаты','комнат'];
+var NOUNS_GUESTS = ['гостей', 'гостя'];
 var DESCRIPTION = ['DescriptionOne', 'DescriptionTwo', 'DescriptionTree', 'DescriptionFour'];
 var CHECK_TIMES = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
@@ -56,7 +58,7 @@ function createOffersArray(count) {
         price: getRandomElementFromArray(PRICES),
         type: getRandomElementFromArray(TYPES),
         rooms: getRandomElementFromArray(ROOMS),
-        guests: getRandomInRange(1, 1001),
+        guests: getRandomInRange(1, 51),
         checkin: getRandomElementFromArray(CHECK_TIMES),
         checkout: getRandomElementFromArray(CHECK_TIMES),
         features: createRandomLengthArray(FEATURES),
@@ -99,7 +101,6 @@ function createPins(arr) {
   mapPins.appendChild(fragment);
 }
 
-
 function createFeaturesTag(features) {
   var featuresHTMLText = '';
   for (var i = 0; i < features.length; i++) {
@@ -108,7 +109,6 @@ function createFeaturesTag(features) {
 
   return featuresHTMLText;
 }
-
 
 function createPhotosHTML(photos) {
   var photosHTMLText = '';
@@ -119,15 +119,15 @@ function createPhotosHTML(photos) {
   return photosHTMLText;
 }
 
-function declOfNum(titles, room) {
+function declOfNumR(titles, room) {
   const cases = [2, 0, 1, 1, 1, 2];
   return titles[(room % 100 > 4 && room % 100 < 20) ? 2 : cases[(room % 10 < 5) ? room % 10 : 5]];
 }
 
 
-function declOfNumM(titles, guests) {
-  const cases = [2, 0, 1, 1, 1, 2];
-  return titles[(guests % 100 > 4 && guests % 100 < 20) ? 2 : cases[(guests % 10 < 5) ? guests % 10 : 5]];
+function declOfNumG(titles, guests) {
+  const cases = [0, 1, 0];
+  return titles[(guests % 100 > 4 && guests % 100 < 20) ? 0 : cases[(guests % 10 < 2) ? guests % 10 : 2]];
 }
 
 function showCard(offer) {
@@ -137,7 +137,7 @@ function showCard(offer) {
   cardElement.querySelector('.popup__text--address').textContent = offer.offer.address;
   cardElement.querySelector('.popup__text--price').textContent = offer.offer.price + '¥/ночь';
   cardElement.querySelector('.popup__type').textContent = RUSSIAN_WORDS[offer.offer.type];
-  cardElement.querySelector('.popup__text--capacity').textContent = offer.offer.rooms + ' ' + declOfNum(['комната', 'комнаты','комнат'] , offer.offer.rooms) + ' для ' + offer.offer.guests + ' ' + declOfNumM(['гостя', 'гостей', 'гостей'], offer.offer.guests);
+  cardElement.querySelector('.popup__text--capacity').textContent = offer.offer.rooms + ' ' + declOfNumR(NOUNS_ROOMS, offer.offer.rooms) + ' для ' + offer.offer.guests + ' ' + declOfNumG(NOUNS_GUESTS, offer.offer.guests);
   cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + offer.offer.checkin + ', выезд до ' + offer.offer.checkout;
   cardElement.querySelector('.popup__features').innerHTML = createFeaturesTag(offer.offer.features);
   cardElement.querySelector('.popup__description').textContent = offer.offer.description;
@@ -148,7 +148,95 @@ function showCard(offer) {
 }
 
 var offers = createOffersArray(DEALS_NEARBY_AMOUNT);
-createPins(offers);
-showCard(offers[0]);
 
+// createPins(offers);
+// showCard(offers[0]);
+var ESC_KEYCODE = 'Escape';
+var ENTER_KEYCODE = 'Enter';
+var NUMPAD_ENTER_KEYCODE = 'NumpadEnter';
 
+var pinMain = document.querySelector('.map__pin--main');
+var inputAddress = document.querySelector('#address');
+
+function activeMap () {
+  document.querySelector('.map').classList.remove('map--faded');
+
+  var mapFilters = document.querySelector('.map__filters');
+
+  var selects = mapFilters.querySelectorAll('select');
+  var fieldsets = mapFilters.querySelectorAll('fieldset');
+
+  for (var i = 0; i < selects.length; i++) {
+    selects[i].disabled = false;
+  }
+
+  for (var j = 0; i < fieldsets.length; j++) {
+    fieldsets[j].disabled = false;
+  }
+};
+
+function activeFrom() {
+  var adForm = document.querySelector('.ad-form');
+  adForm.classList.remove('ad-form--disabled');
+
+  var fieldsets = adForm.querySelectorAll('fieldset');
+
+  for (var i = 0; i < fieldsets.length; i++) {
+    fieldsets[i].disabled = false;
+  }
+}
+
+pinMain.addEventListener('click', function () {
+  activeMap();
+  activeFrom();
+  setCoordMainPin();
+});
+
+pinMain.addEventListener('keydown', function (e) {
+  if (e.code === ENTER_KEYCODE || ENTER_KEYCODE){
+    activeMap();
+    activeFrom()
+  }
+});
+
+var pinMain = document.querySelector('.map__pin--main');
+var inputAddress = document.querySelector('#address');
+
+var PIN_ARROW = 22;
+
+var setCoordMainPin = function (isDisabled) {
+  var pinCoordsX = Math.round(pinMain.offsetLeft + (PIN_WIDTH /  2));
+  var pinCoordsY = Math.round(pinMain.offsetTop + PIN_HEIGHT + PIN_ARROW);
+
+  if (isDisabled){
+    pinCoordsX = Math.round(pinMain.offsetLeft + (PIN_WIDTH / 2));
+    pinCoordsY = Math.round(pinMain.offsetTop + (PIN_WIDTH / 2));
+  }
+
+  inputAddress.value = pinCoordsX + ', ' + pinCoordsY;
+}
+
+setCoordMainPin(true);
+
+var MAX_ROOMS = 100;
+
+var roomNumber = document.querySelector('#room_number');
+var capacity = document.querySelector('#capacity');
+
+var checkCountRoomsAndPeople = function () {
+  var countRooms = roomNumber.value;
+  var countPeople = capacity.value;
+
+  roomNumber.setCustomValidity('');
+  capacity.setCustomValidity('');
+
+  if (countPeople === 0 && countRooms !== MAX_ROOMS) {
+    roomNumber.setCustomValidity('Необходимо не менее 100 комнат');
+  } else if (countRooms === MAX_ROOMS && countPeople !== 0) {
+    capacity.setCustomValidity('Только не для гостей');
+  } else if (countRooms < countPeople && countPeople !== 0) {
+    capacity.setCustomValidity('Кол-ва людей больше чем мест');
+  }
+};
+
+checkCountRoomsAndPeople();
